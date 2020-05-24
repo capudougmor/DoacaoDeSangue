21e209e6c379e2c6235b9b8a81f47962669ec5ee
Douglas Mendes <capudougmor@gmail.com>
2020-05-23 21:53:29 -0300

    Criado conecção com sequelize no banco de dados

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const Sequelize = require('sequelize')
 // configurando o servidor
 const express = require("express")
 const server = express()
@@ -11,16 +12,6 @@ server.use(express.urlencoded({
 }))
 
 //configurar a conexao com o banco de dados
-// const sqlite = require('sqlite3').verbose()
-// var db = sqlite.Database('./donors.sqlite', (err) =>{
-//     if(err){
-//         console.log('Erro ao conectar no banco de dado: ' +err)
-//     }
-//     console.log('Conectado ao banco de dados')
-//     db.close()
-// })
-
-const Sequelize = require('sequelize')
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: './donors.sqlite'
@@ -32,6 +23,20 @@ const sequelize = new Sequelize({
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
+    const donors = sequelize.define('donors', {
+    name: {
+        type: Sequelize.STRING
+    },
+    email: {
+        type: Sequelize.STRING
+    },
+    blood: {
+        type: Sequelize.STRING
+    }
+})
+
+//Criar a tabela
+//donors.sync({force: true})
 
 // configurando a template engine
 const nunjucks = require("nunjucks")
@@ -41,16 +46,19 @@ nunjucks.configure("./",{
 })
 
 //configurar a apresentação da pagina
-server.get("/", function(req, res){
-    db.query("SELECT * FROM donors", function(err, result){
-        if (err) return res.send("Erro na apresentaçãos dos dados!")
-        const donors = result.rows
-        return res.render("index.html", { donors })
+server.get("/", (req, res) =>{
+    donors.findAll({order: [['id', 'DESC']]}).then(function(donors){
+        res.render('index.html', {donors: donors});
     })
+    // donors.findAll("SELECT * FROM donors", function(err, result){
+    //     if (err) return res.send("Erro na apresentaçãos dos dados!")
+    //     const donors = result.rows
+    //     return res.render("index.html", { donors: donors })
+    // })
 }) 
 
-server.post("/",function(req, res){
-    //pegar os dados do formulario
+server.post('/', function(req, res){
+
     const name = req.body.name
     const email = req.body.email
     const blood = req.body.blood
@@ -58,20 +66,15 @@ server.post("/",function(req, res){
     if (name == "" || email == "" || blood == "") {
         return res.send("Todos os campos são obrigatorios!")
     }
-    //colocando os valores no banco de dados 
-    const query = 
-        `INSERT INTO donors ("name", "email", "blood") 
-        VALUES ($1, $2, $3)`
-    const values = [name, email, blood]
 
-    db.query(query, values, function(err) {
-        //fluxo de erro
-        if (err) return res.send("Erro no banco de dados!")
-
-        //fluxo ideal
-        return res.redirect("/")
+    
+    donors.create({name, email, blood}).then(function(){
+        res.redirect('/')
+        //res.send("donors cadastro com sucesso!")
+    }).catch(function(erro){
+        res.send("Erro: donors não foi cadastrado com sucesso!" + erro)
     })
-
+    //res.send("Nome: " + req.body.nome + "<br>Valor: " + req.body.valor + "<br>") 
 })
 
 // ligar a sevidor e permitir o acesso a porta 3000
